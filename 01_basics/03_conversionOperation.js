272cc0a28f2d63518c2b33433ce6ccbece7f1c89
devansh0376 <143001492+devansh0376@users.noreply.github.com>
2025-02-05 10:12:52 +0000

    datatype summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,5 +1,16 @@
 let score=33
 
-console.log(typeof score)
+//console.log(typeof score)
 let new_score =  String(score)
-console.log(typeof new_score)
+//console.log(typeof new_score)
+
+//*********operations *******/
+
+//simple opertaion : + - * / , string add
+
+console.log("1"+2) //12
+console.log(1+3 + "2") // 42
+let n1,n2,n3;
+n1=n2=n3=5 + 5
+//++n1 n1++
+console.table([n1,n2,n3])
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,7 @@
+// console.log(2!=1)
+// console.log("2">=1)
+console.log(null == 0)//false
+console.log(null > 0) // false 
+console.log(null >= 0) // >= or <= convert null in to number treating as 0 so ans is true
+
+// === strict check , it check value as well as data types 
