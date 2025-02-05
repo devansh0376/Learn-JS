94ae85906192387324a7e89f53cdbb157ebb446c
devansh0376 <143001492+devansh0376@users.noreply.github.com>
2025-02-05 11:07:02 +0000

    string in JS

---
+++ 01_basics/05_string.js
@@ -0,0 +1,9 @@
+const name="dev"
+let cnt=76;
+// console.log(name + 76+ " shrimali")
+// console.log(`hii dev ${cnt}`)
+
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name[0])
+console.log(name.charAt(1))
