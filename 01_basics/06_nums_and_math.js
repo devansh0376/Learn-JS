2d015013b5d8cb4f2f226d589458b4386eed4db4
devansh0376 <143001492+devansh0376@users.noreply.github.com>
2025-02-05 11:29:19 +0000

    maths and nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,8 @@
+const score=400
+const balance = new Number(50)
+console.log(balance)
+
+/*********maths********/
+// console.log(Math.min(0,1,2))
+// console.log(Math.round(0.5))
+console.log(Math.random()) //gives value [0,1]
