4fb9ce213983af095b630c713e2a4dfc6094ae68
devansh0376 <143001492+devansh0376@users.noreply.github.com>
2025-02-05 12:00:06 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,10 @@
+//dates
+
+let myDate= new Date()
+// console.log(myDate)
+console.log(myDate.toString())
+//console.log(typeof myDate)
+let newDate=new Date("11-05-2003")
+let currdate= Date.now()
+console.log(newDate.toString())
+console.log(Math.floor(Date.now()/1000))
