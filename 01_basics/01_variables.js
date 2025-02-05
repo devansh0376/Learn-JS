0af8bee7cb4f6680b70801785f3ccd94da2743f8
devansh0376 <143001492+devansh0376@users.noreply.github.com>
2025-02-05 08:37:28 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountID = 1212
+let accountEmail = "dev@gmail.com"
+var accountPassword = "123"
+accountCity= "AMD"
+
+//accountID= 2121
+accountEmail = "ved@gmail.com"
+accountPassword = "321"
+
+console.log(accountCity)
+console.table([accountID,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict" // trate all JS code as newer version
+
+// alert("hoi") // we are using nodejs not browser
+let name="dev" //string data types 
+let age =21 // integer 
+let flag= true //boolean 
+
+// number(2^53) , bigint,string, boolean,null,undefine  , symbol,object
+
+console.log(typeof(true))
