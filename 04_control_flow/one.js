49613bdf55d7ad4bf492d8d0c3ec235b249da378
devansh0376 <143001492+devansh0376@users.noreply.github.com>
2025-02-06 09:23:07 +0000

    controll flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,6 @@
+//if 
+//all operator , === strict check value + data type
+//all same as c++,&&,||,==,===,switch case
+
+// fasle value : false,0,-0,"",NaN,BigInt,undefine,0n
+// truth value : mostly all , "0" , 'false,[],{},function()
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,17 @@
+const month=2
+
+switch(month)
+{
+    case 1:
+        console.log("jan")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    
+    default:
+        break;
+}
